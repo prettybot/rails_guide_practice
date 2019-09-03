2c76599bc10d032002e92c95a2a88eb50fd67c31
lilulu <lilulu@boohee.com>
2019-09-03 14:06:22 +0800

    feat: 更新test case

--- test/models/patient_test.rb
+++ test/models/patient_test.rb
@@ -15,18 +15,10 @@ class PatientTest < ActiveSupport::TestCase
   end
 
   test "auto delete association in the third model" do
-    # binding.pry
-    puts Appointment.count
     @physician_one.patients = Patient.all
-    puts Appointment.count
-    @physician_one.save
-    # binding.pry
     assert_difference 'Appointment.count', -1 do
       @patient_one.destroy
       @physician_one.patients = Patient.all
     end
-    @physician_one.reload
-    puts Appointment.all.map &:as_json
-    puts Appointment.count
   end
 end
